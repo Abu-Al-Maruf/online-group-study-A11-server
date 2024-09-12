e9b6c64859fbb275c7883dcd573a8c3b260a849a
Abu Al Maruf <mdmaruf55667711@gmail.com>
2024-09-13 00:47:30 +0600

    all done

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const port = process.env.PORT || 5000;
 // midlleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://online-group-study-asgn11.web.app",
+    ],
     credentials: true,
   })
 );
